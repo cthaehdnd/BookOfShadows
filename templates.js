16332fc86173ccd2296c7e1cc0b7c1cb55132a1c
ZachRiegel <zjriegel@gmail.com>
2017-01-14 22:20:39 -0500

    foo

--- templates.js
+++ templates.js
@@ -57,7 +57,7 @@ var levelTemplate = _.template(`
 `);
 
 var spellTemplate = _.template(`
-	<div class="spell-title" data-attribute="<%= classes %>" data-title="level:<%= level %>|<%= name %>|<% if(ritual==1){%>[r]<%}%>">
+	<div class="spell-title" data-attribute="<%= classes %>" data-title="l:<%= level %>|n:<%= name %>|s:<%= school %>|<% if(ritual==1){%>[r]<%}%>">
 		<div class="spell-row">
 			<a href="spells.html#<%- name.replace(/ /g, '_') %>"> <%= name %> <% if(ritual==1){%>[r]<%}%></a>
 			<div class="spell-school"> <%= school %> </div>
